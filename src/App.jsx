73edf6d4c17d19c4a6469131a7c8f8a1b8d96741
Mimisax-pixel <wiseonu@gmail.com>
2023-12-29 21:23:36 +0100

    project done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Color from "./color";
+import "./index.css";
+
+function App() {
+
+
+  return (
+    
+      <div>
+        <Color />
+      </div>
+  )
+  }
+export default App;
